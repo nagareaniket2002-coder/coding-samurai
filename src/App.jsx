9856f44948b49f50ff85348d56bf2a89015a8afc
Aniket Nagare <ankitnagare776@gmail.com>
2026-04-23 10:52:38 +0100

    Updated chatn UI and styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,62 +2,52 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [animate, setAnimate] = useState(false);
+  const [messages, setMessages] = useState([
+    { text: "Hello", sender: "bot" },
+  ]);
 
-  const triggerAnimation = () => {
-    setAnimate(true);
-    setTimeout(() => setAnimate(false), 200);
-  };
+  const [input, setInput] = useState("");
 
-  const increase = () => {
-    setCount(count + 1);
-    triggerAnimation();
-  };
+  const sendMessage = () => {
+    if (input.trim() === "") return;
 
-  const decrease = () => {
-    if (count > 0) {
-      setCount(count - 1);
-      triggerAnimation();
-    }
-  };
+    const newMessage = { text: input, sender: "user" };
 
-  const reset = () => {
-    setCount(0);
-    triggerAnimation();
+    setMessages([...messages, newMessage]);
+    setInput("");
+
+    setTimeout(() => {
+      const reply = { text: "Message received ✅", sender: "bot" };
+      setMessages((prev) => [...prev, reply]);
+    }, 1000);
   };
 
   return (
     <div className="container">
-      <div className="glass-card">
-        <h1 className="title">✨ Advanced Counter App</h1>
-        <p className="subtitle">React JS + Modern UI</p>
+      <div className="chat-box">
+        <h2>💬 Chat App</h2>
 
-        <div className={`count-box ${animate ? "pop" : ""}`}>
-          <h2 className={count % 2 === 0 ? "count even" : "count odd"}>
-            {count}
-          </h2>
+        <div className="messages">
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={msg.sender === "user" ? "msg user" : "msg bot"}
+            >
+              {msg.text}
+            </div>
+          ))}
         </div>
 
-        <div className="button-group">
-          <button className="btn increase" onClick={increase}>
-            Increase
-          </button>
+        <div className="input-box">
+          <input
+            type="text"
+            placeholder="Type message..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
 
-          <button
-            className="btn decrease"
-            onClick={decrease}
-            disabled={count === 0}
-          >
-            Decrease
-          </button>
-
-          <button className="btn reset" onClick={reset}>
-            Reset
-          </button>
+          <button onClick={sendMessage}>Send</button>
         </div>
-
-        <p className="note">Counter 0 पेक्षा खाली जाणार नाही.</p>
       </div>
     </div>
   );
